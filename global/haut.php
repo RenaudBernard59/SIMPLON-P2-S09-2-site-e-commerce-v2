5d4e278ef79c2257fbced7d184270cfb14eefd71
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-22 14:56:59 +0200

    Adding minimal PHP-MVC content

---
+++ global/haut.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+
+<html>
+
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+	<title>Tutoriel de Savageman - Créez votre site (presque) complet PHP : architecture MVC et bonnes pratiques !</title>
+
+	<meta http-equiv="Content-Language" content="fr" />
+
+	<link rel="stylesheet" href="style/global.css" type="text/css" media="screen" />
+
+</head>
+
+<body>
+
+	<h1>Créez votre site (presque) complet PHP : architecture MVC et bonnes pratiques !</h1>
+
+<?php include 'global/menu.php'; ?>
+
+	<div id="centre">
+
